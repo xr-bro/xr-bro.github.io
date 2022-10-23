ce3ee98f221d9f5d646194f788afb7e5bf372bf3
onee <onee.me@outlook.com>
2022-10-23 15:18:03 +0800

    fix: fix sidebar

--- docs/.vuepress/components/EmbedCraft.vue
+++ docs/.vuepress/components/EmbedCraft.vue
@@ -4,7 +4,7 @@
       <img id="craft" src="/img/craft.webp" alt="">
     </div>
     <!-- 3.6rem 是 nav bar 的高度 -->
-    <iframe :src="url" style="position:fixed; top:3.6rem; left:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:18;"></iframe>
+    <iframe :src="url"></iframe>
   </div>
 </template>
 <script>
@@ -20,21 +20,38 @@ export default {
 </script>
 <style>
 #background {
-  position:fixed;
+  position: fixed;
   background: white;
-  top:3.6rem;
-  left:0;
-  right:0;
-  width:100%;
-  height:100%;
-  border:none;
-  margin:0;
-  padding:0; z-index:17;
-
+  top: 3.6rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  /* sidebar 的 z-index 是 13，所以这 bg 是 11 */
+  z-index: 11;
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
+iframe {
+  position: fixed;
+  top: 3.6rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  /* sidebar 的 z-index 是 13，所以这 iframe 是 12 */
+  z-index: 12;
+}
+
 #craft {
   /* 直接看 craft loading icon 的效果写死的大小 */
   width: 27px;
